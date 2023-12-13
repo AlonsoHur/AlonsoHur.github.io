9f5263bbf11b8c977f5ed3ee39058fe3387e5523
AlonsoHur <Alonso.Hurtado@alu.uclm.es>
2023-12-14 00:32:17 +0100

    fin inicio + sombreado en scroll

--- js/main.js
+++ js/main.js
@@ -21,3 +21,13 @@ navClose.addEventListener('click', () => {
 
 const navLinks = document.querySelectorAll('.nav_link')
 navLinks.forEach (n => n.addEventListener('click', linkAction))
+
+/* cambiar el fondo de la cabecera */
+const shadowCabecera = () =>{
+    const cabecera = document.getElementById('header')
+    //si el escroll es mas que el la altura de viewport en 50, se añade la clase shadow cabecera al tag de cabecera
+    this.scrollY >= 50 ? header.classList.add('shadow-cabecera')
+                       : header.classList.remove('shadow-cabecera')
+}
+
+window.addEventListener('scroll', shadowCabecera)
